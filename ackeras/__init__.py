ad8508f791a4859aa13e502b63241d4145009214
Andrea Titton <andrea.titton.ext@accurat.it>
2018-09-06 11:09:45 +0200

    added jupyter basic example

--- ackeras/__init__.py
+++ ackeras/__init__.py
@@ -6,4 +6,5 @@ __all__ = [
     'outlier_detection',
     'pipeline',
     'regression',
+    'data_viz',
 ]
--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     name='ackeras',
     version='0.0.1',
     author='Andrea Titton',
+    packages=['ackeras'],
     description='Simple wrapper around Sklearn and Autokeras',
     author_email='andrea.titton@accurat.it',
+    url='https://github.com/accurat/ackeras',
     keywords='autoML',
 )
