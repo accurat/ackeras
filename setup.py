e484b50ed12dd46f6313e81055621895945a7c79
Andrea Titton <andrea.titton.ext@accurat.it>
2018-09-06 10:08:37 +0200

    removed frontend from repository for concistency

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ackeras',
     version='0.0.3',
+    packages=find_packages("ackeras", exclude=["test.py", "images"]),
     author='Andrea Titton',
     author_email='andrea.titton@accurat.it',
     keywords='autoML',
